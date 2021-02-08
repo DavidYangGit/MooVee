a341434d6a07add4cec7cab31a1c5f447ce2f683
David Yang <dyang57@ucmerced.edu>
2021-02-08 05:08:49 -0800

    Completed Week 2

---
+++ MooVee/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  MooVee
+//
+//  Created by David Yang on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
